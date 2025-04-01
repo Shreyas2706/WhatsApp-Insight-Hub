83393541f4fd88c5f192bacd6db0376420a93110
Shreyas2706 <shreyassingh1216@gmail.com>
2025-04-01 20:57:33 +0530

    added new functionality

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ if uploaded_file is not None:
             with col2:
                 # Enhanced DataFrame display
                 st.markdown("#### Detailed User Activity 📋")
-                st.dataframe(new_df)
+                st.dataframe(new_df.style.highlight_max(color='lightgreen', axis=0))
 
         # Emoji Analysis
         st.markdown("## 😀 Emoji Analysis")
